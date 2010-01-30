37fa2c1facb9636c57f69e64ba7cbd252c29c8c8
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2010-01-30 01:48:49 +0000

    JIT: Add framework for invoking a compiled lock.

--- branches/jit/hdrs/boolexp.h
+++ branches/jit/hdrs/boolexp.h
@@ -1,5 +1,10 @@
 #ifndef BOOLEXP_H
 #define BOOLEXP_H
+
+#ifdef HAVE_JIT_JIT_H
+#include <jit/jit.h>
+#endif
+
 #include "copyrite.h"
 #include "chunk.h"
 #include "dbio.h"
@@ -18,13 +23,13 @@ typedef chunk_reference_t boolexp;
 enum { TRUE_BOOLEXP = NULL_CHUNK_REFERENCE };
 
 /* From boolexp.c */
-extern boolexp dup_bool(boolexp b);
-extern int sizeof_boolexp(boolexp b);
-extern int eval_boolexp(dbref player, boolexp b, dbref target);
-extern boolexp parse_boolexp(dbref player, const char *buf, lock_type ltype);
-extern boolexp parse_boolexp_d(dbref player, const char *buf, lock_type ltype,
+boolexp dup_bool(boolexp b);
+int sizeof_boolexp(boolexp b);
+int eval_boolexp(dbref player, boolexp b, dbref target);
+boolexp parse_boolexp(dbref player, const char *buf, lock_type ltype);
+boolexp parse_boolexp_d(dbref player, const char *buf, lock_type ltype,
                                int derefs);
-extern void free_boolexp(boolexp b);
+void free_boolexp(boolexp b);
 boolexp getboolexp(PENNFILE *f, const char *ltype);
 void putboolexp(PENNFILE *f, boolexp b);
 enum u_b_f {
@@ -33,5 +38,9 @@ enum u_b_f {
   UB_MEREF /**< Use dbrefs or "me" if the object is the player arg
               from unparse_boolexp.() For @decompile. */
 };
-extern char *unparse_boolexp(dbref player, boolexp b, enum u_b_f flag);
+char *unparse_boolexp(dbref player, boolexp b, enum u_b_f flag);
+
+#ifdef USE_JIT
+jit_function_t compile_boolexp(dbref thing, boolexp b);
+#endif
 #endif                          /* BOOLEXP_H */

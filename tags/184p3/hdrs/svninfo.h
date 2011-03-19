68adf473d947473ca88b1f8612119e7d32972e5b
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2011-03-19 01:31:29 +0000

    Pull back p3 tag

--- tags/184p3/hdrs/svninfo.h
+++
@@ -1,3 +0,0 @@
-#define SVNREVISION "$Rev$"
-#define SVNDATE "$Date$"
-/* Built at 20110316172004 */
--- tags/184p3/hdrs/version.h
+++
@@ -1,4 +0,0 @@
-#define VERSION "1.8.4"
-#define PATCHLEVEL "3"
-#define PATCHDATE "[03/18/2011]"
-#define NUMVERSION 1008004003

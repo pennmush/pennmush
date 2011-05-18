8e52ff0d7749ca4b34a3be3f3cb926c1ef7e5d27
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2011-05-18 23:55:32 +0000

    Add file with control routines for the ssl slave.
    git-svn-id: https://pennmush.googlecode.com/svn/trunk@1010 ba372814-4f39-11de-9ad6-1127a62b9fcd

---
+++ src/ssl_master.c
@@ -0,0 +1,94 @@
+/** SSL slave controller related code. */
+
+#include "copyrite.h"
+#include "config.h"
+
+#ifdef I_SYS_TYPES
+#include <sys/types.h>
+#endif
+#include <signal.h>
+#ifdef I_UNISTD
+#include <unistd.h>
+#endif
+#ifdef I_SYS_SOCKET
+#include <sys/socket.h>
+#endif
+
+#include <errno.h>
+#include <stdio.h>
+
+#include "conf.h"
+#include "externs.h"
+#include "log.h"
+#include "mysocket.h"
+#include "ssl_slave.h"
+#include "parse.h"
+
+#include "confmagic.h"
+
+
+pid_t ssl_slave_pid = -1;
+enum ssl_slave_state ssl_slave_state = SSL_SLAVE_DOWN;
+
+extern int maxd;
+
+/** Create a new SSL slave.
+ * \param port The port to listen on for SSL connections.
+ * \return 0 on success, -1 on failure
+ */
+int
+make_ssl_slave(void)
+{
+  if (ssl_slave_state != SSL_SLAVE_DOWN) {
+    do_rawlog(LT_ERR, "Attempt to start ssl slave when a copy is already running.");
+    return -1;
+  }
+
+  if ((ssl_slave_pid = fork()) == 0) {
+    /* Set up and exec ssl_slave */
+    char *args[9];
+    int n, errfd;
+
+    /* Close extranenous file descriptors */
+    errfd = fileno(stderr);
+    for (n = 0; n < maxd; n++) {
+      if (n == errfd)
+	continue;
+      close(n);
+    }
+
+    /* Set up arguments to the slave */
+    args[0] = "ssl_slave";
+    args[1] = options.socket_file;
+    args[2] = SSL_IP_ADDR;
+    args[3] = strdup(unparse_integer(options.ssl_port));
+    args[4] = options.ssl_private_key_file;
+    args[5] = options.ssl_ca_file;
+    args[6] = options.ssl_require_client_cert ? "1" : "0";
+    args[7] = strdup(unparse_integer(options.keepalive_timeout));
+    args[8] = NULL;
+
+    execv("./ssl_slave", args);
+    penn_perror("execing ssl slave");
+    return EXIT_FAILURE;
+  } else if (ssl_slave_pid < 0) {
+    do_rawlog(LT_ERR, "Failure to fork ssl_slave: %s", strerror(errno));
+    return -1;
+  } else {
+    ssl_slave_state = SSL_SLAVE_RUNNING;
+    do_rawlog(LT_ERR, "Spawning ssl_slave, communicating over %s, pid %d.",
+	      options.socket_file, ssl_slave_pid);
+    return 0;
+  }  
+
+  return -1;
+}
+
+void
+kill_ssl_slave(void)
+{
+  if (ssl_slave_pid > 0) {
+    kill(ssl_slave_pid, SIGTERM);
+    ssl_slave_state = SSL_SLAVE_DOWN;
+  }
+}

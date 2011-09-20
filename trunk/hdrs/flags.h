8890cd6cd819715b858fecc12956d80d41390e20
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2011-09-20 23:39:02 +0000

    Issue 618: Windows compile fix.

--- trunk/hdrs/flags.h
+++ trunk/hdrs/flags.h
@@ -9,6 +9,8 @@
 #include "mushtype.h"
 #include "ptab.h"
 #include "dbio.h"
+/* Remove when confmagic's inline #defines get shifted to config.h */
+#include "confmagic.h"
 
 typedef struct flag_info FLAG;
 

e7a8ee7daff83d5f244ab723fb8c838eda4f3e1f
talvo@talvo.com <talvo@talvo.com@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2012-05-19 19:09:08 +0000

    ANSI normal didn't reset bg/fg colors.

--- src/markup.c
+++ src/markup.c
@@ -908,8 +908,11 @@ define_ansi_data(ansi_data *store, const char *str)
       /* This is explicitly normal, it'll never be colored */
       store->bits = 0;
       store->offbits = ~0;
-      store->fg[0] = '\0';
-      store->bg[0] = '\0';
+      store->fg[0] = 'd';
+      store->bg[0] = 'D';
+      store->fg[1] = '\0';
+      store->bg[1] = '\0';
+
       break;
     case 'f':                  /* flash */
       store->bits |= CBIT_FLASH;

pennmush/pennmush

commit edfd62de6e56f6b9b1a6960f4bcc19307289ffd0

Remove ! as a valid fg/bg separator for ansi().
git-svn-id: https://pennmush.googlecode.com/svn/trunk@1542 ba372814-4f39-11de-9ad6-1127a62b9fcd

src/markup.c
@@ -148,7 +148,7 @@ FUNCTION(fun_colors)
     /* Return color info for a specific color */
     char color[COLOR_NAME_LEN];
     char *p = args[0];
-    while (*p && (*p == '+' || *p == '!' || *p == '/'))
+    while (*p && (*p == '+' || *p == '/'))
       p++;
     if (!*p || (!valid_color_name(p) && !valid_color_hex(p+1))) {
       safe_str(T("#-1 INVALID COLOR"), buff, bp);
@@ -509,7 +509,7 @@ write_ansi_letters(const ansi_data *cur, char *buff, char **bp)
       }
     }
     if (cur->bg[0] == '+' || cur->bg[0] == '#') {
-      retval += safe_chr('!', buff, bp);
+      retval += safe_chr('/', buff, bp);
       retval += safe_str(cur->bg, buff, bp);
     }
   }
@@ -978,7 +978,6 @@ define_ansi_data(ansi_data *store, const char *str)
       break;
     case '#':
     case '+':
-    case '!':
     case '/':
       goto new_ansi;
     }
@@ -994,7 +993,7 @@ new_ansi:
     case '+':
       /* Color names. */
       name = str+1;
-      while (*str && *str != '/' && *str != '!' && *str != TAG_END) {
+      while (*str && *str != '/' && *str != TAG_END) {
         str++;
       }
       strncpy(buff, name, str-name);
@@ -1012,7 +1011,7 @@ new_ansi:
     case '#':
       /* Hex colors. */
       name = str+1;
-      while (*str && *str != '/' && *str != '!' && *str != TAG_END) {
+      while (*str && *str != '/' && *str != TAG_END) {
         str++;
       }
       strncpy(buff, name, str-name);
@@ -1023,7 +1022,6 @@ new_ansi:
       snprintf(ptr, COLOR_NAME_LEN, "#%s", buff);
       break;
     case '/':
-    case '!':
       ptr = store->bg;
       str++;
       break;

game/txt/hlp/pennfunc.hlp
@@ -581,9 +581,6 @@ See also: powers(), orlpowers(), POWERS LIST, @power
   those colors, PennMUSH will downgrade the color to the closest fit
   that your client can understand.
   
-  When using the newer color names/hexcodes, you can also use ! instead of
-  / to separate the foreground and background colors.
- 
 See also: ANSI, COLOR, @sockset, colorstyle, colors()
 & ANSI2
   Old-style valid color codes are:

hdrs/svninfo.h
@@ -1,3 +1,3 @@
 #define SVNREVISION "$Rev$"
 #define SVNDATE "$Date$"
-/* Built at 20120704192409 */
+/* Built at 20120705001035 */

commit 08786308b10d3a0a6eed2114ebcbe3c2f399c108

Unbreak ansi broken in r1542. Woops.
git-svn-id: https://pennmush.googlecode.com/svn/trunk@1565 ba372814-4f39-11de-9ad6-1127a62b9fcd

src/markup.c
@@ -148,7 +148,7 @@ FUNCTION(fun_colors)
     /* Return color info for a specific color */
     char color[COLOR_NAME_LEN];
     char *p = args[0];
-    while (*p && (*p == '+' || *p == '/'))
+    while (*p && (*p == '+' || *p == '/' || *p == '!'))
       p++;
     if (!*p || (!valid_color_name(p) && !valid_color_hex(p + 1))) {
       safe_str(T("#-1 INVALID COLOR"), buff, bp);
@@ -510,7 +510,7 @@ write_ansi_letters(const ansi_data *cur, char *buff, char **bp)
       }
     }
     if (cur->bg[0] == '+' || cur->bg[0] == '#') {
-      retval += safe_chr('/', buff, bp);
+      retval += safe_chr('!', buff, bp);
       retval += safe_str(cur->bg, buff, bp);
     }
   }
@@ -1003,7 +1003,7 @@ new_ansi:
     case '+':
       /* Color names. */
       name = str + 1;
-      while (*str && *str != '/' && *str != TAG_END) {
+      while (*str && *str != '/' && *str != TAG_END && *str != '!') {
         str++;
       }
       strncpy(buff, name, str - name);
@@ -1022,7 +1022,7 @@ new_ansi:
     case '#':
       /* Hex colors. */
       name = str + 1;
-      while (*str && *str != '/' && *str != TAG_END) {
+      while (*str && *str != '/' && *str != TAG_END && *str != '!') {
         str++;
       }
       strncpy(buff, name, str - name);
@@ -1033,6 +1033,7 @@ new_ansi:
       snprintf(ptr, COLOR_NAME_LEN, "#%s", buff);
       break;
     case '/':
+    case '!':
       ptr = store->bg;
       str++;
       break;
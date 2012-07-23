1149e8810cee4b0e6eec8ca1af4759f880675cb5
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2012-07-23 21:55:45 +0000

    Fix a memory leak in LOGOUT

--- trunk/src/bsd.c
+++ trunk/src/bsd.c
@@ -2678,7 +2678,7 @@ process_commands(void)
           break;
         case CRES_LOGOUT:
           logout_sock(cdesc);
-          break;
+	  /* Fall through to free input buffer */
         case CRES_OK:
           cdesc->input.head = t->nxt;
           if (!cdesc->input.head)

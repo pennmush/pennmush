9081a1d72b74a30297dabf86eaf7741b96480bd1
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2012-10-16 01:17:30 +0000

    Fix a test error caused by deprecating unescaped commas in some functions.
    git-svn-id: https://pennmush.googlecode.com/svn/trunk@1647 ba372814-4f39-11de-9ad6-1127a62b9fcd

--- test/testnull.pl
+++ test/testnull.pl
@@ -5,5 +5,5 @@ test('null.3', $god, 'think null(a,b,c)', '');
 test('null.4', $god, 'think null(pemit(#1,test))', 'test');
 test('null.5', $god, 'think @@()', '');
 test('null.6', $god, 'think @@(a)', '');
-test('null.7', $god, 'think @@(a,b,c)', '');
+test('null.7', $god, 'think @@({a,b,c})', '');
 test('null.8', $god, 'think @@(pemit(#1,test))', '');

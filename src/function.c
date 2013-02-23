9fd4c9a25190362bf01961a81da4c76a4699c4d2
talvo@talvo.com <talvo@talvo.com@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2013-02-23 08:15:10 +0000

    Change @@() to take INT_MAX args to stop unescaped comma warning.
    git-svn-id: https://pennmush.googlecode.com/svn/trunk@1694 ba372814-4f39-11de-9ad6-1127a62b9fcd

--- src/function.c
+++ src/function.c
@@ -157,7 +157,7 @@ FUNALIAS faliases[] = {
  * add_function().
  */
 FUNTAB flist[] = {
-  {"@@", fun_null, 1, -1, FN_NOPARSE},
+  {"@@", fun_null, 1, INT_MAX, FN_NOPARSE},
   {"ABS", fun_abs, 1, 1, FN_REG | FN_STRIPANSI},
   {"ACCENT", fun_accent, 2, 2, FN_REG},
   {"ACCNAME", fun_accname, 1, 1, FN_REG},

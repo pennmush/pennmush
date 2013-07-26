e39c9d15af010689914de25ee060af4989a0664e
zetafunction@gmail.com <zetafunction@gmail.com@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2013-07-26 05:51:54 +0000

    Update CityHash/MurmurHash3/SpookyHash implementations.
    This patch incorporates the latest upstream code for both CityHash and
    SpookyHash and switches the MurmurHash3 implementation to the x64
    optimized one.

--- hdrs/hash_function.h
+++ hdrs/hash_function.h
@@ -13,12 +13,9 @@
 #include <stdint.h>
 #endif  /* HAVE_STDINT_H_ */
 
-/* Note that CityHash doesn't have an implementation that returns a 32-bit hash.
- * This is just CityHash64 using only the lowest 32 bits. */
-uint32_t city_hash(const char *buf, int len);
-/* TODO: Conditionally compile x86 or x64 version depending on platform. */
-uint32_t murmur3_x86_32(const char *key, int len);
-uint32_t spooky_hash32(const char *message, int len);
-uint32_t jenkins_hash(const char *k, int len);
+uint32_t city_hash(const char *buf, int len, uint64_t seed);
+uint32_t murmur3_hash(const char *key, int len, uint64_t seed);
+uint32_t spooky_hash(const char *message, int len, uint64_t seed);
+uint32_t jenkins_hash(const char *k, int len, uint64_t seed);
 
 #endif  /* __HASH_FUNCTION_H_ */

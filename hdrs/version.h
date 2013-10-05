5f896928e5ccc78ffecbff5cf5647a6b7bcefd58
allthecoolkidshaveone <allthecoolkidshaveone@ba372814-4f39-11de-9ad6-1127a62b9fcd>
2013-10-05 05:16:30 +0000

    Bump version in prepration for a release

--- hdrs/version.h
+++ hdrs/version.h
@@ -1,4 +1,4 @@
 #define VERSION "1.8.5"
-#define PATCHLEVEL "4"
-#define PATCHDATE "[11/09/2012]"
-#define NUMVERSION 1008005004
+#define PATCHLEVEL "5"
+#define PATCHDATE "[?? / ?? /2013]"
+#define NUMVERSION 1008005005

3ff35de3c852dc11f1fef07f9427655c1d48db68
gilmarion <rcheng@digitallyinked.com>
2016-04-24 15:27:36 -0400

    Update mushtype.h

--- hdrs/mushtype.h
+++ hdrs/mushtype.h
@@ -56,7 +56,7 @@ typedef struct lock_list lock_list;
 #define MAX_ITERS 100
 
 /* max length of command argument to process_command */
-#define MAX_COMMAND_LEN 4096
+#define MAX_COMMAND_LEN 8192
 #define BUFFER_LEN ((MAX_COMMAND_LEN)*2)
 #define MAX_ARG 63
 

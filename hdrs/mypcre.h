31d2a0254f61eae246bd933bd3db0d5619033624
Shawn <shawnw.mobile@gmail.com>
2017-09-22 22:05:08 -0700

    Since PCRE is required, skip ifdef checks for it.

--- hdrs/mypcre.h
+++ hdrs/mypcre.h
@@ -10,9 +10,6 @@ struct pcre_extra;
 void set_match_limit(struct pcre_extra *);
 struct pcre_extra *default_match_limit(void);
 
-#ifdef HAVE_PCRE
-
-#ifdef HAVE_PCRE_H
 #include <pcre.h>
 
 #ifndef PCRE_STUDY_JIT_COMPILE
@@ -22,9 +19,5 @@ struct pcre_extra *default_match_limit(void);
 extern int pcre_study_flags;
 extern int pcre_public_study_flags;
 
-#else
-#error "You appear to have a system PCRE library but not the pcre.h header."
-#endif
+#endif                          /* End of pcre.h */
 
-#endif /* !HAVE_PCRE */
-#endif /* End of pcre.h */

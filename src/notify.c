2592924e70eec14191d03622755f403c9bfc2ff2
Harry Cordewener <admin@twilightdays.org>
2021-03-25 14:16:40 -0500

    PR Changes - need help on utf8_to_latin1 for MSG_LATIN1 (2)

--- src/notify.c
+++ src/notify.c
@@ -770,7 +770,8 @@ render_string(const char *message, int output_type)
     do_log(LT_WIZ, GOD,GOD,"P: %s",p);
     latin1 = utf8_to_latin1(p, -1, &len, 0, "utf8.to-latin1");
     do_log(LT_WIZ, GOD,GOD,"LATIN1: %s",p);
-    safe_strl(latin1,len,buff,&buff);
+    bp = buff;
+    safe_strl(latin1,len,buff,&bp);
     mush_free(latin1,"utf8.to-latin1");
     return buff;
   }
